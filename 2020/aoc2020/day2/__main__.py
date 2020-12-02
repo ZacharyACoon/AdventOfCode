27642cc8691d5f490327d1f7f7bdf77e307a508e
ZacharyACoon <69929375+ZacharyACoon@users.noreply.github.com>
2020-12-01 21:32:21 -0800

    mangled solve

--- 2020/aoc2020/day2/__main__.py
+++ 2020/aoc2020/day2/__main__.py
@@ -4,4 +4,25 @@ from aoc2020.common.utils import type_per_line
 
 
 if __name__ == "__main__":
-    pass
+    input = type_per_line(puzzle_input.from_file_arg(), str)
+
+    valid = 0
+    for line in input:
+        rule, password = line.split(":")
+
+        amount = rule[:-2]
+        requirement = rule[-1]
+        minimum, maximum = amount.split("-")
+        pos1 = int(minimum)
+        pos2 = int(maximum)
+
+        p1 = password[pos1] == requirement
+        p2 = password[pos2] == requirement
+
+        print(requirement, pos1, pos2, password)
+        print(pos1, password[pos1], pos2, password[pos2])
+        if (p1 and not p2) or (p2 and not p1):
+            print("added")
+            valid += 1
+
+    print(valid)
