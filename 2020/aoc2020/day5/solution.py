b5cfa2eaea84e4178ad5e34e76cbdb4237555959
ZacharyACoon <69929375+ZacharyACoon@users.noreply.github.com>
2020-12-05 00:34:00 -0800

    fancy

--- 2020/aoc2020/day5/solution.py
+++ 2020/aoc2020/day5/solution.py
@@ -1,48 +1,62 @@
 from aoc2020.common import puzzle_input
 
 
+def vector_row(path):
+    offset = 0
+    span = 128
+    for direction in path:
+        if direction == "B":
+            offset += span // 2
+
+        span //= 2
+    return offset
+
+
+def vector_column(path):
+    offset = 0
+    span = 8
+    for direction in path:
+        if direction == "R":
+            offset += span // 2
+
+        span //= 2
+    return offset
+
+
+def calculate_seat_id(row, column):
+    return row * 8 + column
+
+
+def path_to_cartesian(path):
+    row = vector_row(path[:7])
+    column = vector_column(path[-3:])
+    return row, column
+
+
+def process_path(path):
+    return calculate_seat_id(*path_to_cartesian(path))
+
+
 def part1(input):
-    for letters in input:
-        row = letters[:7]
-
-        min_row = 0
-        max_row = 127
-        for letter in row:
-            half = (max_row - min_row + 1) // 2
-            if letter == "F":
-                max_row -= half
-            else:
-                min_row += half
-
-        seat = letters[-3:]
-        min_seat = 0
-        max_seat = 7
-        for letter in seat:
-            half = (max_seat - min_seat + 1) // 2
-            if letter == "R":
-                min_seat += half
-            else:
-                max_seat -= half
-        seatid = min_row*8+min_seat
-        yield seatid
+    input = input.splitlines()
+    return max(map(process_path, input))
 
 
 def part2(input):
-    l = sorted(part1(input))
+    input = input.splitlines()
+    l = sorted(map(process_path, input))  # ordered seat list
+
     i = 0
-    v = 21
-    while True:
-        if l[i] != v:
-            print(v)
-            break
-
-
+    v = l[i]
+    while i < len(l):
+        if v != l[i]:
+            return v
+        i += 1
+        v += 1
 
 
 if __name__ == "__main__":
-    input = puzzle_input.from_arg_file().splitlines()
-    # input = ["BFFFBBFRRR", "FFFBBBFRRR", "BBFFBBFRLL"]
+    input = puzzle_input.from_arg_file()
 
-
-    # print("Part 1:", max(part1(input)))
+    print("Part 1:", part1(input))
     print("Part 2:", part2(input))
