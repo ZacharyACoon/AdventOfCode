6dfe019d2298e037817d7d2c68ffb3c9a1732bb9
ZacharyACoon <69929375+ZacharyACoon@users.noreply.github.com>
2020-12-04 20:23:15 -0800

    humph

--- 2020/aoc2020/day1/__main__.py
+++ 2020/aoc2020/day1/__main__.py
@@ -2,8 +2,7 @@ from aoc2020.common import puzzle_input
 from . import solution
 
 
-if __name__ == "__main__":
-    input = puzzle_input.from_arg_file()
+input = puzzle_input.from_arg_file()
 
-    print("Part 1:", solution.solve1(input))
-    print("Part 2:", solution.solve2(input))
+print("Part 1:", solution.solve1(input))
+print("Part 2:", solution.solve2(input))
