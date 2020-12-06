752a54f2a6d62058eb618fb363a96a313e85bd66
ZacharyACoon <69929375+ZacharyACoon@users.noreply.github.com>
2020-12-05 21:00:37 -0800

    refine template

--- 2020/aoc2020/template/__main__.py
+++
@@ -1,9 +0,0 @@
-from aoc2020.common import puzzle_input
-from . import solution
-
-
-if __name__ == "__main__":
-    input = puzzle_input.from_arg_file()
-
-    print("Part 1:", )
-    print("Part 2:", )
--- 2020/aoc2020/template/solution.py
+++ 2020/aoc2020/template/solution.py
@@ -1,4 +1,16 @@
 
 
-def template():
+def part1(input):
     pass
+
+
+def part2(input):
+    pass
+
+
+if __name__ == "__main__":
+    from aoc2020.common import puzzle_input
+    input = puzzle_input.from_arg_file()
+
+    print("Part 1:", part1(input))
+    print("Part 2:", part2(input))
