2c892125bf121ac4d8ad3579cdeaacc03933cf56
ZacharyACoon <69929375+ZacharyACoon@users.noreply.github.com>
2020-12-15 02:08:40 -0800

    cleaner?

--- 2020/aoc2020/day15/solution.py
+++ 2020/aoc2020/day15/solution.py
@@ -8,72 +8,50 @@ def parse_input(input):
     return [int(i) for i in input.split(",")]
 
 
+def van_eck_sequence(start_numbers=None, turns=2020):
+    spoken = {}
+    if start_numbers:
+        spoken.update((number, [offset]) for offset, number in enumerate(start_numbers))
+
+    number = list(spoken.keys())[-1]
+    for turn in range(len(spoken), turns):
+        if number in spoken:
+            previous_utterance_turn = spoken[number]
+            if len(previous_utterance_turn) == 1:
+                number = 0
+            else:
+                number = previous_utterance_turn[-1] - previous_utterance_turn[-2]
+
+        if number not in spoken:
+            spoken[number] = [turn]
+        else:
+            spoken[number].append(turn)
+
+    return number
+
+
 class Solution(Solution):
     def part1(self, input):
         input = parse_input(input)
-        spoken = {}
-        turn = 0
-        last = 0
-        while turn < 2020:
-            if turn < len(input):
-                number = input[turn]
-            else:
-                number = last
+        return van_eck_sequence(input, 2020)
 
-            if number in spoken:
-                if len(spoken[number]) == 1:
-                    number = 0
-                else:
-                    turns = spoken[number]
-                    number = turns[-1] - turns[-2]
-
-            if number not in spoken:
-                spoken[number] = [turn]
-            else:
-                spoken[number].append(turn)
-
-            last = number
-            turn += 1
-        return last
-
-    def part2(self, input, turns=30000000):
+    def part2(self, input):
         input = parse_input(input)
-        spoken = {}
-        number = 0
-        spoken.update((value, [i]) for i, value in enumerate(input))
-        for turn in range(len(input), turns):
-            if number in spoken:
-                when = spoken[number]
-                if len(when) == 1:
-                    number = 0
-                else:
-                    number = when[-1] - when[-2]
-
-            if number not in spoken:
-                spoken[number] = [turn]
-            else:
-                spoken[number].append(turn)
-
-        return number
+        return van_eck_sequence(input, 30000000)
 
 
 class Test(TimedTestCase):
     examples = puzzle_input.from_examples(__file__)  # list of stripped str
 
-    # def test1_part1_example1(self):
-    #     self.assertEqual(436, Solution().part1("0,3,6"))
+    def test1_part1_example1(self):
+        self.assertEqual(436, Solution().part1("0,3,6"))
 
     def test2_part2_example1(self):
-        self.assertEqual(436, Solution().part2("0,3,6", 2020))
-
-    def test3_part2_example2(self):
-        self.assertEqual(175594, Solution().part2("0,3,6", 30000000))
+        self.assertEqual(175594, Solution().part2("0,3,6"))
 
 
 if __name__ == "__main__":
     # unittest.main()
     input = puzzle_input.from_arg_file()
-    print(input)
-    solution = Solution()
-    print("Part 1:", solution.part1(input))
-    print("Part 2:", solution.part2(input))
+    print("Part 1:", Solution().part1(input))
+    print("Part 2:", Solution().part2(input))
