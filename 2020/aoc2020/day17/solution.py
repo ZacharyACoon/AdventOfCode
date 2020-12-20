77e66c9e870032e9b0c6f5368fdee37aeb84d5ca
ZacharyACoon <69929375+ZacharyACoon@users.noreply.github.com>
2020-12-20 01:34:04 -0800

    .

--- 2020/aoc2020/day17/solution.py
+++ 2020/aoc2020/day17/solution.py
@@ -1,6 +1,7 @@
 import unittest
 from aoc2020.common import puzzle_input
-
+from scipy.spatial.kdtree import KDTree
+import numpy as np
 
 """
 # active
@@ -8,113 +9,146 @@ from aoc2020.common import puzzle_input
 """
 
 
-def parse_active_coordinates(data):
+def parse_active_coordinates(data, dimensions=2):
     data = data.splitlines()
     coordinates = []
     for y, line in enumerate(data):
         for x, char in enumerate(line):
             if char == "#":
-                coordinates.append((x, y, 0))
-    x1 = 0
-    x2 = len(data[0])
-    y1 = 0
-    y2 = len(data)
-    z1 = 0
-    z2 = 1
-    return coordinates, x1, x2, y1, y2, z1, z2
+                coordinates.append((*(x, y), *[0 for _ in range(dimensions-2)]))
+    boundaries = [
+        0, len(data[0]),
+        0, len(data),
+    ]
+    for _ in range(dimensions - 2):
+        boundaries.append(0)
+        boundaries.append(1)
+    return coordinates, boundaries
 
 
-def get_neighbor_coordinates(coordinates, things=None):
-    if len(coordinates) == 1:
-        for thing in range(coordinates[0]-1, coordinates[0]+2):
-            yield *things, thing
+def generate_tesseract(coordinates, boundaries):
+    pts = np.array(coordinates)
+    tree = KDTree(pts, leafsize=1000)
+    active_list = coordinates
+    active_dict = dict((coordinate, True) for coordinate in coordinates)
+    return tree, active_dict, active_list, boundaries
+
+
+def recursively_iterate_boundaries(boundaries, layer=None):
+    if layer is None:
+        layer = []
+    minimum = boundaries[0]
+    maximum = boundaries[1]
+    if len(boundaries) > 2:
+        for thing in range(minimum-1, maximum+1):
+            yield from recursively_iterate_boundaries(boundaries[2:], layer=(*layer, thing))
     else:
-        for thing in range(coordinates[0]-1, coordinates[0]+2):
-            yield from get_neighbor_coordinates(coordinates[1:], things=(thing,))
+        for thing in range(minimum-1, maximum+1):
+            yield *layer, thing
 
 
-def count_neighbors(tesseract, x, y, z):
-    neighbors = 0
-    for coordinate in get_neighbor_coordinates(x, y, z):
-        if coordinate in tesseract:
-            neighbors += 1
+def count_neighbors(tesseract, coordinate):
+    tree, active_dict, active_list, boundaries = tesseract
+    # don't count given coordinate itself as a neighbor
+    neighbor_offsets = tree.query_ball_point(coordinate, 1.999)
+    neighbors = len([o for o in neighbor_offsets if active_list[o] != coordinate])
+    print(neighbors)
+    # for debugging neighbors found at each point
+    # neighbors = 0
+    # for o in neighbor_offsets:
+    #     print("active list", active_list)
+    #     if active_list[o] != coordinate:
+    #         print("ignore self")
+    #         continue
+    #     neighbors += 1
     return neighbors
 
 
-def flip_tesseract(tesseract, x1, x2, y1, y2, z1, z2):
-    nx1, nx2, ny1, ny2, nz1, nz2 = x1, x2, y1, y2, z1, z2
-    new_tesseract = tesseract.copy()
-    for z in range(z1-1, z2+1):
-        for y in range(y1-1, y2+1):
-            for x in range(x1-1, x2+1):
-                coordinate = (x, y, z)
-                if (x, y, z) in tesseract:
-                    # active
-                    neighbors = count_neighbors(tesseract, *coordinate)
-                    if not (2 <= neighbors <= 3):
-                        del new_tesseract[coordinate]
-                else:
-                    # inactive
-                    neighbors = count_neighbors(tesseract, *coordinate)
-                    if neighbors == 3:
-                        # update bounds
-                        if x == x1: nx1 = x1 - 1
-                        if x == x2: nx2 = x2 + 1
-                        if y == y1: ny1 = y1 - 1
-                        if y == y2: ny2 = y2 + 1
-                        if z == z1: nz1 = z1 - 1
-                        if z == z2: nz2 = z2 + 1
-                        new_tesseract[coordinate] = True
-    return new_tesseract, nx1, nx2, ny1, ny2, nz1, nz2
+def flip_tesseract(tesseract: (KDTree, list)):
+    tree, active_dict, active_list, boundaries = tesseract
+    new_active_dict = active_dict.copy()
+    new_boundaries = boundaries.copy()
+    for coordinate in recursively_iterate_boundaries(boundaries):
+        neighbors = count_neighbors(tesseract, coordinate)
+        # print(coordinate, neighbors)
+        if coordinate in active_dict:
+            if not 2 <= neighbors <= 3:
+                del new_active_dict[coordinate]
+        else:
+            if neighbors == 3:
+                new_active_dict[coordinate] = True
+                # expand boundaries of tesseract if needed
+                for c, o in enumerate(range(0, len(boundaries), 2)):
+                    if coordinate[c] <= boundaries[o]:
+                        new_boundaries[o] = boundaries[o] - 1
+                    elif coordinate[c] >= boundaries[o+1]:
+                        new_boundaries[o+1] = boundaries[o+1] + 1
+    new_active_list = list(new_active_dict.keys())
+    pts = np.array(new_active_list)
+    new_tree = KDTree(pts, leafsize=1000)
+    return new_tree, new_active_dict, new_active_list, new_boundaries
 
 
-def print_tesseract(tesseract, x1, x2, y1, y2, z1, z2):
-    for z in range(z1, z2):
-        # print(f"z={z}")
-        lines = []
-        for y in range(y1, y2):
+def count_active(tesseract):
+    tree, active_dict, active_list, boundaries = tesseract
+    return len(active_list)
+
+
+def print_tesseract(tesseract, layer=None):
+    tree, active_dict, active_list, boundaries = tesseract
+    if layer is None:
+        layer = []
+    if len(boundaries) // 2 - len(layer) > 2:
+        o = len(boundaries)-len(layer)*2-2
+        minimum = boundaries[o]
+        maximum = boundaries[o+1]
+        for thing in range(minimum, maximum):
+            print_tesseract(tesseract, layer=(*layer, thing))
+    else:
+        plate = ""
+        for y in range(boundaries[2], boundaries[3]):
             row = ""
-            for x in range(x1, x2):
-                row += "#" if (x, y, z) in tesseract else "."
-            lines.append(row)
-        # print("\n".join(lines) + "\n")
-    # print()
+            for x in range(boundaries[0], boundaries[1]):
+                if (x, y, *reversed(layer)) in active_dict:
+                    row += "#"
+                else:
+                    row += "."
+            plate += row + "\n"
+        print(*layer)
+        print(plate)
 
 
-def cycle_tesseract(tesseract, bounds, count):
-    for c, _ in enumerate(range(count)):
-        tesseract, *bounds = flip_tesseract(tesseract, *bounds)
-        # print(f"After {c+1} cycle:")
-        # print(f"Active: ", len(tesseract))
-        print_tesseract(tesseract, *bounds)
-    return tesseract, bounds
-
-
-def part1(data, cycles=6):
-    coordinates, *bounds = parse_active_coordinates(data)
-    tesseract = dict((coordinate, True) for coordinate in coordinates)
-    print_tesseract(tesseract, *bounds)
-    tesseract, bounds = cycle_tesseract(tesseract, bounds, cycles)
-    return len(tesseract)
+def part1(data):
+    active_coordinates, boundaries = parse_active_coordinates(data, 3)
+    tesseract = generate_tesseract(active_coordinates, boundaries)
+    for _ in range(6):
+        tesseract = flip_tesseract(tesseract)
+        print_tesseract(tesseract)
+        quit()
+    return count_active(tesseract)
 
 
 def part2(data):
-    for thing in get_neighbor_coordinates((3, 3, 3)):
-        print(thing)
+    active_coordinates, boundaries = parse_active_coordinates(data, 4)
+    tesseract = generate_tesseract(active_coordinates, boundaries)
+    print_tesseract(tesseract)
+    print("cycle")
+    tesseract = flip_tesseract(tesseract)
+    print_tesseract(tesseract)
 
 
 class Test(unittest.TestCase):
     examples = puzzle_input.from_examples(__file__)  # list of stripped str
 
-    # def test1_part1_example1(self):
-    #     self.assertEqual(112, part1(self.examples[0]), 6)
-
-    def test2_part2_example(self):
-        part2(self.examples[0])
+    def test1_part1_example1(self):
+        self.assertEqual(112, part1(self.examples[0]))
+    #
+    # def test2_part2_example(self):
+    #     part2(self.examples[0])
 
 
 if __name__ == "__main__":
     # unittest.main()
     data = puzzle_input.from_arg_file()
-    # print("Part 1:", part1(data))
+    print("Part 1:", part1(data))
     # print("Part 2:", part2(data))
