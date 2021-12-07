2a64fdcbe5a27f458ef1a07c9b0d8b18a7014e2f
ZacharyACoon <69929375+ZacharyACoon@users.noreply.github.com>
2021-12-07 05:51:04 -0800

    day7

--- 2021/python/aoc2021/day7/solution.py
+++ 2021/python/aoc2021/day7/solution.py
@@ -1,25 +1,71 @@
 import unittest
+from aoc2021.common.testing import TimedTestCase
 from aoc2021.common import puzzle_input
+import math
+# 21 57
+
+
+def parse_crabs(data):
+    return list(map(int, data.split(",")))
+
+
+def map_position_to_count_and_get_range(crabs):
+    counts = {}
+    l = math.inf
+    u = -math.inf
+    for position in crabs:
+        if position < l:
+            l = position
+        if position > u:
+            u = position
+
+        if position not in counts:
+            counts[position] = 1
+        else:
+            counts[position] += 1
+    return counts, l, u
 
 
 def part1(data):
-    pass
+    crabs = parse_crabs(data)
+    position_to_count, l, u = map_position_to_count_and_get_range(crabs)
+
+    least_fuel_alignment = 0
+    least_fuel_alignment_cost = math.inf
+    for position in range(l, u+1):
+        fuel_required = 0
+        for crab_position, crab_count in position_to_count.items():
+            fuel_required += abs(position - crab_position) * crab_count
+        if fuel_required < least_fuel_alignment_cost:
+            least_fuel_alignment_cost = fuel_required
+            least_fuel_alignment = position
+    return least_fuel_alignment_cost
 
 
 def part2(data):
-    pass
+    crabs = parse_crabs(data)
+    position_to_count, l, u = map_position_to_count_and_get_range(crabs)
+    least_fuel_alignment_cost = math.inf
+    for position in range(l, u+1):
+        fuel_required = 0
+        for crab_position, crab_count in position_to_count.items():
+            distance = abs(position - crab_position)
+            distance_change_cost = distance * (distance + 1) // 2
+            fuel_required += distance_change_cost * crab_count
+
+        if fuel_required < least_fuel_alignment_cost:
+            least_fuel_alignment_cost = fuel_required
+    return least_fuel_alignment_cost
 
 
-class Test(unittest.TestCase):
-    examples = puzzle_input.from_examples(__file__)  # list of stripped str
+class Test(TimedTestCase):
+    example = "16,1,2,0,4,2,7,1,2,14"
 
     def test1_part1_example1(self):
-        example = self.examples[0]
-        self.assertEqual(0, part1(example))
+        self.assertEqual(37, part1(self.example))
 
     def test2_part2_example1(self):
-        example = self.examples[0]
-        self.assertEqual(0, part2(example))
+        self.assertEqual(168, part2(self.example))
 
 
 if __name__ == "__main__":
