1116bb5eec033d3447c8e3c432415ec05a57aff7
ZacharyACoon <69929375+ZacharyACoon@users.noreply.github.com>
2021-12-10 19:13:23 -0800

    day9 part1?

--- 2021/python/aoc2021/day9/solution.py
+++ 2021/python/aoc2021/day9/solution.py
@@ -1,9 +1,43 @@
 import unittest
 from aoc2021.common import puzzle_input
+import math
+
+
+def parse(data):
+    l = len(data)
+    width = 0
+    heights = []
+    for i, c in enumerate(data):
+        if c != "\n":
+            width += 1
+            heights.append(int(c))
+        else:
+            width = 0
+
+    return heights, width
+
+
+def is_low_point(heights, width, p):
+    w = width
+    l = len(heights)
+    adjacent_points = p-w, p-1, p+1, p+w
+    adjacent_points = filter(lambda v: 0 <= v < l, adjacent_points)
+    center_height = heights[p]
+    for adjacent_point in adjacent_points:
+        adjacent_point_height = heights[adjacent_point]
+        if adjacent_point_height <= center_height:
+            return False
+    else:
+        return True
 
 
 def part1(data):
-    pass
+    heights, width = parse(data)
+    risk_level = 0
+    for point in range(len(heights)):
+        if is_low_point(heights, width, point):
+            risk_level += heights[point] + 1
+    return risk_level
 
 
 def part2(data):
@@ -15,11 +49,12 @@ class Test(unittest.TestCase):
 
     def test1_part1_example1(self):
         example = self.examples[0]
-        self.assertEqual(0, part1(example))
+        print(example)
+        self.assertEqual(15, part1(example))
 
-    def test2_part2_example1(self):
-        example = self.examples[0]
-        self.assertEqual(0, part2(example))
+    # def test2_part2_example1(self):
+    #     example = self.examples[0]
+    #     self.assertEqual(0, part2(example))
 
 
 if __name__ == "__main__":
