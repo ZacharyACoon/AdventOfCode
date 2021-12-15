a8c8d531f433cf2aa645b673291052431ab2b3e5
ZacharyACoon <69929375+ZacharyACoon@users.noreply.github.com>
2021-12-14 18:36:16 -0800

    day14 refined

--- 2021/python/aoc2021/day14/solution.py
+++ 2021/python/aoc2021/day14/solution.py
@@ -1,4 +1,6 @@
 import unittest
+import math
+import time
 from aoc2021.common import puzzle_input
 
 
@@ -25,105 +27,33 @@ def parse(data):
         ab, c = line.split(" -> ")
         ab = tuple(ab)
         rules[ab] = c
-
-    return template, rules, pair_counts
-
-
-def step(template, rules):
-    old_template = template
-    template = []
-    for o in range(len(old_template)-1):
-        a = old_template[o]
-        b = old_template[o+1]
-        template.append(a)
-        ab = tuple((a, b))
-        template.append(rules[ab])
-    template.append(b)
-    return template
+    return pair_counts, rules
 
 
-def count_elements(template):
-    counts = dict()
-    for c in template:
-        if c not in counts:
-            counts[c] = 1
-        else:
-            counts[c] += 1
-    return counts
-
-import math
-import time
-
-
-def part1(data, steps=10):
-    template, rules, pair_counts = parse(data)
-    start_time = time.time()
-    for s in range(steps):
-        template = step(template, rules)
-        step_time = time.time()
-        print(f"Step {s} took {step_time-start_time} seconds.")
-
-    counts = count_elements(template)
-    least_common = None
-    least_common_count = math.inf
-    most_commont = None
-    most_common_count = -math.inf
-    for c, count in counts.items():
-        if count < least_common_count:
-            least_common = c
-            least_common_count = count
-        if count > most_common_count:
-            most_commont = c
-            most_common_count = count
-
-    return most_common_count - least_common_count
-
-
-def step2(pair_counts, rules):
+def step(pair_counts, rules):
     old_pair_counts = pair_counts
-    pair_counts = dict()
-
-    changes = {}
+    pair_counts = pair_counts.copy()
     for (ab), count in old_pair_counts.items():
         a, b = ab
         c = rules[ab]
         ac = a, c
         cb = c, b
-        if ab not in changes:
-            changes[ab] = 0
-        changes[(a, b)] -= count
+        pair_counts[ab] -= count
 
-        if ac not in changes:
-            changes[ac] = 0
-        changes[(a, c)] += count
+        if ac not in pair_counts:
+            pair_counts[ac] = 0
+        pair_counts[ac] += count
 
-        if cb not in changes:
-            changes[cb] = 0
-        changes[(c, b)] += count
-
-    # print("old_pair_counts")
-    # print(old_pair_counts)
-    # print("changes")
-    # print(changes)
-
-    for pair, change in changes.items():
-        if pair not in old_pair_counts:
-            count = 0
-        else:
-            count = old_pair_counts[pair]
-        count += change
-        if count != 0:
-            pair_counts[pair] = count
-
-    # print("pair_counts")
-    # print(pair_counts)
+        if cb not in pair_counts:
+            pair_counts[cb] = 0
+        pair_counts[cb] += count
 
     return pair_counts
 
 
-def count_elements2(pairs_count):
+def count_elements_in_pairs(pairs_count):
     elements = {}
-    for ab, count in pairs_count.items():
+    for (ab), count in pairs_count.items():
         a, b = ab
         if a not in elements:
             elements[a] = 0
@@ -132,40 +62,38 @@ def count_elements2(pairs_count):
         if b not in elements:
             elements[b] = 0
         elements[b] += count
+
+    for element, count in elements.items():
+        elements[element] = math.ceil(count / 2)
+
     return elements
 
-import math
 
-def part2(data, steps=40):
-    template, rules, pair_counts = parse(data)
-    start_time = time.time()
-    for s in range(steps):
-        pair_counts = step2(pair_counts, rules)
-        step_time = time.time()
-        print(f"step {s+1} took {step_time - start_time} seconds.")
-
-    element_counts = count_elements2(pair_counts)
-    # print(element_counts)
-
-    least_common = None
+def determine_commons(counts):
     least_common_count = math.inf
     most_common_count = -math.inf
+    for c, count in counts.items():
+        least_common_count = min(least_common_count, count)
+        most_common_count = max(most_common_count, count)
+    return least_common_count, most_common_count
 
-    for c, count in element_counts.items():
-        count = math.ceil(count / 2)
-        if count < least_common_count:
-            least_common = c
-            least_common_count = count
-        if count > most_common_count:
-            most_common = c
-            most_common_count = count
 
-    # print(most_common)
-    # print(most_common_count)
-    # print(least_common)
-    # print(least_common_count)
-
+def part1(data, steps=10):
+    pair_counts, rules = parse(data)
+    # print(pair_counts)
+    start_time = time.time()
+    for s in range(steps):
+        pair_counts = step(pair_counts, rules)
+        step_time = time.time()
+        # print(f"Step {s+1}, {step_time-start_time:{0:.8f}} seconds.")
+        # print(pair_counts)
+    element_counts = count_elements_in_pairs(pair_counts)
+    least_common_count, most_common_count = determine_commons(element_counts)
     return most_common_count - least_common_count
+
+
+def part2(data, steps=40):
+    return part1(data, steps)
 
 
 class Test(unittest.TestCase):
@@ -173,7 +101,7 @@ class Test(unittest.TestCase):
 
     def test1_part1_example1(self):
         example = puzzle_input.from_example(__file__, 1)
-        self.assertEqual(1588, part2(example, steps=10))
+        self.assertEqual(1588, part1(example, steps=10))
 
     def test2_part2_example1(self):
         example = self.examples[0]
