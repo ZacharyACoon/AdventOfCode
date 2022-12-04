836ef8ede1de32b32cbf9b6f6c005a9b36759b6f
ZacharyACoon <zacharyacoon@gmail.com>
2022-12-04 04:32:22 -0800

    year2022day03 solved

--- source/python/aoc/year2022/day03/test.py
+++ source/python/aoc/year2022/day03/test.py
@@ -1,21 +1,21 @@
 import unittest
 from pathlib import Path
-import solution
+from . import solution
 
 parent = Path(__file__).parent
 
 
 class Test(unittest.TestCase):
-    with open("example_input1.txt") as f:
+    with open(parent / "example_input1.txt") as f:
         example_input1 = f.read()
 
     def test1_part1(self):
         output = solution.part1(self.example_input1)
-        # assert output ==
+        assert output == 157
 
     def test2_part2(self):
         output = solution.part2(self.example_input1)
-        # assert output ==
+        assert output == 70
 
 
 if __name__ == "__main__":
