d6cf206fd2ab8feca966d96e4aacdaa46293b1bd
ZacharyACoon <zacharyacoon@gmail.com>
2022-12-04 15:31:02 -0800

    year202204 setup

---
+++ source/python/aoc/year2022/day04/solution.py
@@ -0,0 +1,8 @@
+
+
+def part1(example_input):
+    pass
+
+
+def part2(example_input):
+    pass
---
+++ source/python/aoc/year2022/day04/test.py
@@ -0,0 +1,14 @@
+from aoc.common.testcase import TestCase
+from . import solution
+
+
+class Test(TestCase):
+    def test1_part1(self):
+        pass
+        # output = solution.part1(self.example_input1)
+        # assert output == 0
+
+    def test2_part2(self):
+        pass
+        # output = solution.part2(self.example_input1)
+        # assert output == 0
